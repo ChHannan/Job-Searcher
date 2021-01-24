fcdf59364a4d541e76bf591e647a60938d412a61
CH.Hannan <ch.hannan15@gmail.com>
2021-01-24 21:09:57 +0500

    Added loading bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {LoadingBarHttpClientModule} from '@ngx-loading-bar/http-client';
 import {AuthenticationInterceptorService} from 'src/app/services/authentication-interceptor.service';
 import {AppRoutingModule} from './app-routing.module';
 
@@ -20,7 +21,8 @@ import {SharedModule} from './shared-module/shared.module';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    SharedModule,
+    LoadingBarHttpClientModule,
   ],
   providers: [{
     provide: HTTP_INTERCEPTORS, useClass: AuthenticationInterceptorService, multi: true
